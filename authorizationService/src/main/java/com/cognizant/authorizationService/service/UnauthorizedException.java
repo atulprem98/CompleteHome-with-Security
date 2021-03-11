d0e8f6dc07dfeaa1fe9c11fd9a02534bcab8d1a7
Atul Premachandran <69211828+atulprem98@users.noreply.github.com>
2021-03-11 22:18:42 +0530

    Add files via upload

---
+++ authorizationService/src/main/java/com/cognizant/authorizationService/service/UnauthorizedException.java
@@ -0,0 +1,13 @@
+package com.cognizant.authorizationService.service;
+
+/** Service class */
+@SuppressWarnings("serial")
+public class UnauthorizedException extends RuntimeException {
+	/**
+	 * @param message
+	 */
+	public UnauthorizedException(String message) {
+		super(message);
+	}
+
+}
